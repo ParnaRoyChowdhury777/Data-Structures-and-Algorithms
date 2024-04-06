397552217ce7ed620f612664b8882758c488cdce
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-04-07 03:32:31 +0530

    Add code for printing a pattern with stars in a diamond shape

--- Patterns/Pattern20.cpp
+++ Patterns/Pattern20.cpp
@@ -9,31 +9,31 @@ int main()
     
     for(int i=1; i<=n; i++)
     {
-        for(int j=1; j<=n-i+1; j++)
+        for(int j=1; j<=i; j++)
         {
             cout<<"*";
         }
-        for(int k=1; k<=2*n-2*(n-i+1); k++)
+        for(int k=1; k<=2*(n - i); k++)
         {
             cout<<" ";
         }
-        for(int m=1; m<=n-i+1; m++)
+        for(int m=1; m<=i; m++)
         {
             cout<<"*";
         }
         cout<<endl;
     }
-    for(int i=1; i<=n; i++)
+    for(int i=1; i<=n-1; i++)
     {
-        for(int j=1; j<=i; j++)
+        for(int j=1; j<=n-i; j++)
         {
             cout<<"*";
         }
-        for(int k=1; k<=2*(n-i); k++)
+        for(int k=1; k<=2*i; k++)
         {
             cout<<" ";
         }
-        for(int m=1; m<=i; m++)
+        for(int m=1; m<=n-i; m++)
         {
             cout<<"*";
         }
